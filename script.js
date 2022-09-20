631ab27bc7553efb28a9ae8d4a12e64543867637
dev-rahim <rahimuddin5915@gmail.com>
2022-09-20 12:28:27 +0600

    compleate javascript class add and deletion and image replace

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('ok connected!');
+const panels = document.querySelectorAll('.panel');
+
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClass()
+        panel.classList.add('active')
+    })
+});
+
+function removeActiveClass() {
+    for (const panel of panels) {
+        panel.classList.remove('active');
+    }
+}
